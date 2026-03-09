bb0d481765bad5681ce04aeacd6e9942af7fa447
Rafael-Duarte-Silva <rafaelddduante@gmail.com>
2026-03-09 13:21:38 -0300

    fix: improve URL params code verification

--- apps/api/src/urls/schemas/urls-params.schema.ts
+++ apps/api/src/urls/schemas/urls-params.schema.ts
@@ -1,7 +1,12 @@
 import z from "zod";
 
 export const urlParamsSchema = z.object({
-    code: z.string().trim().min(1).max(10),
+    code: z
+        .string()
+        .trim()
+        .min(1)
+        .max(10)
+        .regex(/^[0-9A-Za-z]+$/),
 });
 
 export type UrlParamsSchema = z.infer<typeof urlParamsSchema>;
